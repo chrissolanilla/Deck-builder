03fc710417e2d4f28efb18370cb823886eaac053
Christopher Solanilla <chrissolanilla@gmail.com>
2024-10-20 14:21:56 -0400

    fixes null error on start game if deck size is less than 5

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,11 @@
 extends VBoxContainer
-
+var deck: Array[CardMetaData] = []
 func _ready() -> void:
+	#load the savegame.data and see if we have atleast 5 cards in the file.
+	deck = DeckManager.loadCurrentDeck(deck)
+	#this condition fixes the null error when playing game
+	if deck.size()< 5:
+		$PlayButton.disabled = true
 	$PlayButton.pressed.connect(_on_play_pressed)
 	$DeckButton.pressed.connect(_on_deck_pressed)
 
